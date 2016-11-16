14a320dac277929c0885cd67f9d647892acd07e1
iiborn <abbasgit91@gmail.com>
2016-11-16 10:57:10 +0330

    Update Plist.lua

--- plugins/Plist.lua
+++ plugins/Plist.lua
@@ -2,7 +2,7 @@ do
 
 local function run(msg, matches)
   if matches[1] == 'plist' or 'planlist' then
-    send_document(get_receiver(msg), "/root/black/data/me/payment.webp", ok_cb, false)
+    send_document(get_receiver(msg), "/root/M/data/me/payment.webp", ok_cb, false)
       return "For more information please Contact this robot \n\n@ibornbot"
 end
 end
