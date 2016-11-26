8bf35fe2772db3b9569514d8f597be07b987536a
iiborn <abbasgit91@gmail.com>
2016-11-26 12:57:42 +0330

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -2,7 +2,7 @@ do
 
 local function run(msg, matches)
   if matches[1] == 'nerkh' or 'cast' then
-    send_document(get_receiver(msg), "/root/black/data/me/nerkh.webp", ok_cb, false)
+    send_document(get_receiver(msg), "/root/M/data/me/nerkh.webp", ok_cb, false)
       return "لینک قیمت های بات ضد اسپم و محافظ گروه\n7روز رایگان برای تست بات\n30روزه 3هزارتومان\n60روز 5 هزارتومان\nنامحدود 10هزارتومان\nهزینه به صورت شارژ یا کارت به کارت پذیرفته میشود\n\nاطلاعات بیشتر @ibornbot"
 end
 end
